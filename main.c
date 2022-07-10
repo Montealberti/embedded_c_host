8d3e889261d6ffa3818fa69927201c957545d9a5
Montealberti <montealberti.solis@outlook.com>
2022-07-10 07:32:11 -0500

    vPadding function added

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 
 #include<stdio.h>
 #include<stdint.h>
+#include"main.h"
 
 /**
  * @brief Function Prototypes Section
@@ -30,6 +31,12 @@ void vStringTest(void);
  *
  */
 void vFloatingPoint(void);
+
+/**
+ * @brief Padding example
+ *
+ */
+void vPadding(void);
 
 /**
  * @brief sizeof command with the different data types of the host/target
@@ -81,15 +88,62 @@ void vFloatingPoint(void)
 }
 
 /**
+ * @brief Padding example
+ *
+ */
+void vPadding(void)
+{
+	uint32_t	packetValue;
+	printf("Enter the 32bit packet value: ");
+	scanf("%X", &packetValue);
+
+	struct Packet packet;
+	uint8_t *ptr;
+
+	ptr = (uint8_t*)&packet;
+
+	packet.crc 			= (uint8_t)(packetValue & 0x3);
+	packet.status		= (uint8_t)((packetValue >> 2) & 0x1);
+	packet.payload		= (uint16_t)((packetValue >> 3) & 0xFFF);
+	packet.bat			= (uint8_t)((packetValue >> 15) & 0x7);
+	packet.sensor		= (uint8_t)((packetValue >> 18) & 0x7);
+	packet.longAddr		= (uint8_t)((packetValue >> 21) & 0xFF);
+	packet.shortAddr	= (uint8_t)((packetValue >> 29) & 0x3);
+	packet.addrMode		= (uint8_t)((packetValue >> 31) & 0x1);
+
+	printf("crc         : %#x \n",	packet.crc);
+	printf("status      : %#x \n",	packet.status);
+	printf("payload     : %#x \n",	packet.payload);
+	printf("bat         : %#x \n",	packet.bat);
+	printf("sensor      : %#x \n",	packet.sensor);
+	printf("longAddr    : %#x \n",	packet.longAddr);
+	printf("shortAddr   : %#x \n",	packet.shortAddr);
+	printf("addrMode    : %#x \n",	packet.addrMode);
+
+
+	printf("\n\nSize of structure is: %llu\n",	sizeof(packet));
+
+	printf("Memory address            Content \n");
+	printf("================================= \n");
+
+	for(uint32_t i = 0; i<sizeof(packet); i++)
+	{
+		printf("%p    %X\n", ptr, *ptr);
+		ptr++;
+	}
+}
+
+/**
  * @brief Main Function
  *
  * @return int value with error code, 0 = success
  */
 int main(void)
 {
-	vFloatingPoint();
+	vPadding();
 
     printf("\n\n**************   Press enter to exit this application   **************\n");
+	getchar();
 	getchar();
 
 	return 0;
